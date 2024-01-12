a0df6cec9563c3638e062302bcfa2fc2d46f81da
Mark Njoroge <marknjo7@gmail.com>
2024-01-12 17:29:32 +0300

    add Register zod Schema for signup form

--- schemas/index.ts
+++ schemas/index.ts
@@ -9,3 +9,16 @@ export const LoginSchema = z.object({
   }),
 });
 export type TLoginSchema = z.infer<typeof LoginSchema>;
+
+export const RegisterSchema = z.object({
+  name: z.string().min(1, {
+    message: 'Username is required',
+  }),
+  email: z.string().email({
+    message: 'Email is required',
+  }),
+  password: z.string().min(6, {
+    message: 'Password must be more than 6 characters',
+  }),
+});
+export type TRegisterSchema = z.infer<typeof RegisterSchema>;
