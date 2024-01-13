ab1215d153bac3e43d2a074ad72e5b6b0606af9e
Mark Njoroge <marknjo7@gmail.com>
2024-01-13 16:56:04 +0300

    implement user register - still there is ToDo action

--- actions/register.ts
+++ actions/register.ts
@@ -1,14 +1,30 @@
 'use server';
 
+import bcrypt from 'bcryptjs';
 import { RegisterSchema, TRegisterSchema } from '@/schemas';
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: TRegisterSchema) => {
-  console.table(values);
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
     return { error: 'Invalid field values!' };
   }
 
-  return { success: 'Login details sent' };
+  const { name, email, password } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: 'Email already in use!' };
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  await db.user.create({ data: { name, email, password: hashedPassword } });
+
+  // TODO: send verification token email
+
+  return { success: 'Your account has been created!' };
 };
