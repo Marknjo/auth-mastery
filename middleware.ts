a80624a51ad220059704ba2fe23dc21539cdc6e6
Mark Njoroge <marknjo7@gmail.com>
2024-01-13 17:00:26 +0300

    setup nestjs middleware for authentication and authrization - part 1

---
+++ middleware.ts
@@ -0,0 +1,43 @@
+import NextAuth from 'next-auth';
+import authConfig from '@/auth.config';
+
+import {
+  DEFAUTL_LOGIN_REDIRECT,
+  authRoutes,
+  apiAuthPrefix,
+  publicRoutes,
+} from '@/middleware.rountes';
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  console.log('\n🚩🚩 middleware');
+
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isAPiAuthPrefix = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isAPiAuthPrefix) return null;
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAUTL_LOGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL('/auth/login', nextUrl));
+  }
+
+  return null;
+});
+
+// Optionally, don't invoke Middleware on some paths
+// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
