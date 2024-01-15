ae374699ced16de463c52c31af5ee09a4bf31310
Mark Njoroge <marknjo7@gmail.com>
2024-01-15 12:11:41 +0300

    add NewPasswordSchema & ResetSchema zod schemas & types

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,19 @@
 import * as z from 'zod';
 
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6, {
+    message: 'Password must be more than 6 characters',
+  }),
+});
+export type TNewPasswordSchema = z.infer<typeof NewPasswordSchema>;
+
+export const ResetSchema = z.object({
+  email: z.string().email({
+    message: 'Email is required',
+  }),
+});
+export type TResetSchema = z.infer<typeof ResetSchema>;
+
 export const LoginSchema = z.object({
   email: z.string().email({
     message: 'Email is required',
