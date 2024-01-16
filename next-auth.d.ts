0dbe6c78285031cf5670b564cb73855af87e9458
Mark Njoroge <marknjo7@gmail.com>
2024-01-16 12:23:33 +0300

    refactor next-auth.s.ts by adding ExtendUser type & more custom fields

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,10 +1,14 @@
 import { UserRole } from '@prisma/client';
 import { type DefaultSession } from 'next-auth';
 
+export type ExtendUser = {
+  role: UserRole;
+  isTwoFactorEnabled: boolean;
+  isOAuth: boolean;
+} & DefaultSession['user'];
+
 declare module 'next-auth' {
   interface Session {
-    user: {
-      role: UserRole;
-    } & DefaultSession['user'];
+    user: ExtendUser;
   }
 }
